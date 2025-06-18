e6f8a5415c12012d1e810f24171d1320c9a8db37
drunker <drunker@51cashbox.com>
2025-06-18 19:05:43 +0800

    增加软体插件

---
+++ Assets/Standard Assets/ThirdParty/2DSoftBody/Code/Editor/SoftSpriteInspector.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(SoftSprite))]
+public class SoftSpriteInspector : Editor
+{
+	SerializedProperty textureProp;
+	SerializedProperty scaleProp;
+  //  SerializedProperty colorProp;
+    private Vector2 scaleVal;
+
+	void OnEnable()
+	{
+		textureProp = serializedObject.FindProperty("Sprite");
+		scaleProp = serializedObject.FindProperty("Scale");
+      //  colorProp = serializedObject.FindProperty("Color");
+
+        SetValues();
+	}
+
+	private void SetValues()
+	{
+		scaleVal = scaleProp.vector2Value;
+	}
+
+	private bool CheckForUpdate()
+	{
+		if (scaleProp.vector2Value != scaleVal)
+		{
+			SetValues();
+			return true;
+		}
+		return false;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		EditorGUILayout.PropertyField(textureProp, new GUIContent("Sprite"));
+		EditorGUILayout.PropertyField(scaleProp, new GUIContent("Scale"));
+       // EditorGUILayout.PropertyField(colorProp, new GUIContent("Color"));
+        serializedObject.ApplyModifiedProperties();
+
+		if (GUILayout.Button("Update") || CheckForUpdate())
+		{
+			foreach (var target in targets)
+			{
+				var softSprite = (SoftSprite)target;
+				softSprite.ForceUpdate();
+			}
+		}
+	}
+}
